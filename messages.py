8d06e8545bc089e0102df67db007c2fe705c6844
Justin Clark <jclarksummit@gmail.com>
2023-08-31 13:16:29 -0400

    initial commit

---
+++ messages.py
@@ -0,0 +1,9 @@
+welcome_messages = [
+    "Ope! Hey {user} lemme sneak past ya real quick and give ya a Michigan fact:",
+    "Hey bud, {user} asked for a Michigan fact so here ya go:",
+    "Sure pal, here's a Michigan fact for {user}:",
+    "Ah jeez, {user} asked for a Michigan fact so I guess I'll give ya one:",
+    "Hey {user}, here's a Michigan fact:",
+    "Hey {user}, did you know this about the great state of Michigan?:",
+    "Did someone say Michigan? Here's a fact for my buddy {user}:",
+]
