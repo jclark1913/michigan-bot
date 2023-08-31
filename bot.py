cb0ab73589bdc82538f1a101117beb03ec7171fc
Justin Clark <jclarksummit@gmail.com>
2023-08-31 14:12:00 -0400

    Add reply method to on_message event

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
-import os
 import aiohttp
+import os
 
 import discord
 from discord.ext import commands
 
 from dotenv import load_dotenv
 
+from messages import welcome_messages
+
 import random
 
-from messages import welcome_messages
 
 # Load token from env
 load_dotenv()
@@ -42,17 +43,20 @@ async def on_message(message):
         user = message.author.display_name
         fact = await get_fact_from_api()
         welcome_message = get_welcome_message(user)
-        await message.channel.send(welcome_message + "\n\n" + fact)
+        await message.reply(welcome_message + "\n\n" + fact)
 
 
 async def get_fact_from_api():
+    """Makes a request to the Michigan facts API and returns a random fact"""
     async with aiohttp.ClientSession() as session:
         async with session.get("https://michigan-facts-ts.vercel.app/facts") as resp:
             data = await resp.json()
             fact = data["randomFact"]["content"]
             return fact
 
+
 def get_welcome_message(user):
+    """Returns a random welcome message"""
     return random.choice(welcome_messages).format(user=user)
 
 
